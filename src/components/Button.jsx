3cc34898ab8187eab5cd671e4084462290665f75
Remy Denimal <remy.denimal@gmail.com>
2024-12-31 01:46:57 +0100

    new sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,17 @@
-const Button = ({ label, iconURL}) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7
-     py-4 my-3 font-montserrat text-lg leading-none bg-coral-red
-     rounded-full text-white hover:font-semibold">
+    <button className={`flex justify-center items-center gap-2 px-7
+     py-4 my-3 border font-montserrat text-lg leading-none hover:font-semibold
+      ${
+        backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}`
+        : 'bg-coral-red text-white'
+      } rounded-full w-full'}`}>
         {label}   
-        <img
+        {iconURL && <img
             src={iconURL}
             alt="Arrow right icon"
-            className="ml-2 rounded-full w-5 h-5" />
+            className="ml-2 rounded-full w-5 h-5" 
+        />}
     </button>
   )
 }
